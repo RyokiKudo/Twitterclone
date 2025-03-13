7224bbd55e81dc76508d865fd0b4a74ed00f9215
RyokiKudo <zenoninfinity46@gmail.com>
2025-03-14 00:54:38 +0900

    Tailwind CSS の設定を追加し、グローバルスタイルを定義

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
